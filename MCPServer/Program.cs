f5c973f67dbaf6b9d73bccfa21019aa06bf853c0
Dmitry Kazennov <kazennovdmitriy@gmail.com>
2026-05-29 10:07:44 +0200

    Changed Console.SetOut for the application. This will prevent third-party libraries from dumping console output into stdout used for the MCP protocol. But MCP SDK StdioServerTransport uses Console.OpenStandardInput() directly and is not affected.

--- MCPServer/Program.cs
+++ MCPServer/Program.cs
@@ -9,6 +9,14 @@ class Program
 {
     static async Task<int> Main(string[] args)
     {
+        // This will prevent third-party libraries from dumping console output into stdout used for the MCP protocol.
+        // But MCP SDK StdioServerTransport uses Console.OpenStandardInput() directly and is not affected.
+        // https://github.com/modelcontextprotocol/csharp-sdk/blob/69a7fd39/src/ModelContextProtocol.Core/Server/StdioServerTransport.cs#L28-L34
+        Console.SetOut(new StreamWriter(Console.OpenStandardError())
+        {
+            AutoFlush = true
+        });
+        
         var builder = Host.CreateApplicationBuilder(args);
 
         builder.Services.AddMcpServer()
